4e8955951a15226730caaba30d3c2f2a80faae96
= <=>
2024-08-17 11:30:22 -0500

    Agrega app.js para config de routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const artistsRoutes = require('./routes/artistsRoutes');
+const songsRoutes = require('./routes/songsRoutes');
+const artists_songsRoutes = require('./routes/artists_songsRoutes');
+
 const app = express();
 
+// Middleware para procesar JSON
 app.use(express.json());
 
+// Configuración de las rutas
+app.use('/artists', artistsRoutes);
+app.use('/songs', songsRoutes);
+app.use('/artists_songs', artists_songsRoutes);
+
+// Exportación del módulo app para que pueda ser usado en index.js
 module.exports = app;
